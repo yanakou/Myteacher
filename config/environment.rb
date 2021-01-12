bb51244c5a8566143f37fb164ce7fa2f1efab554
yanakou <ky50315101@outlook.jp>
2021-01-13 07:55:24 +0900

    connect to heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# rootへのルーティング作成 Herokuでアプリケーションにアクセスした時、最初に開くページをRails側で設定する
+config.assets.compile = true
+config.assets.initialize_on_precompile=false
