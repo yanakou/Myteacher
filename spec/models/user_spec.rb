116b15886039592f2bed33f5560b444578f396b9
yanakou <ky50315101@outlook.jp>
2021-02-04 07:33:03 +0900

    #9 update beforeでテストをDRY化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
- it "名前、メールアドレスがある場合、有効である" do
-  user = User.new(
-    name:"Example User", 
-    email:"user@example.com"
-  )
-
-  expect(user).to be_valid
+  before do
+    @user = User.new(
+      name:"Example User", 
+      email:"user@example.com")
   end
+
+  it "名前、メールアドレスがある場合、有効である" do
+    expect(@user).to be_valid
+  end
+    
 end
