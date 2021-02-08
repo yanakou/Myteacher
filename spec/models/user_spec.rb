64e756c7947a1a164d2acd018869ffd55401d281
yanakou <ky50315101@outlook.jp>
2021-02-08 13:03:45 +0900

    #15 Add フォーマットが正しくない場合のテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,11 +33,21 @@ RSpec.describe User, type: :model do
 
   it "emailのフォーマットが正しい場合、有効である" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org 
-      first.last@foo.jp alice+bob@baz.cn]
+                        first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       expect(@user).to be_valid, "#{valid_address.inspect} should be valid"
     end
   end
+
+  it "emailのフォーマットが正しくない場合、無効である" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. 
+                        foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      expect(@user).to_not be_valid, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
 
 end
