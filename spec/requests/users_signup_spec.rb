8f6b8476625e587a5ac815f19849db30d30791ad
yanakou <ky50315101@outlook.jp>
2021-02-15 16:59:28 +0900

    #44 Add 有効なユーザー登録時のテストを追加

--- spec/requests/users_signup_spec.rb
+++ spec/requests/users_signup_spec.rb
@@ -1,15 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "UsersSignup", type: :request do
-  describe "GET /signup" do
-    let(:subject){User.create(name:  "",
+  describe "無効なユーザー登録" do
+    it "ユーザー数は変わらない" do
+      get signup_path
+      expect do
+      post users_path, params:{user: {name:  "",
                               email: "user@invalid",
                               password:              "foo",
-                              password_confirmation: "bar" )}
-    it "invalid signup information" do
-      get signup_path
-      expect{subject}.to_not change{User.count}
+                              password_confirmation: "bar" }}
+      end.to_not change{User.count}
       expect(response).to render_template("users/new")
     end
   end
+
+  describe "有効なユーザー登録" do
+    it "ユーザー数が1増える" do
+      get signup_path
+      expect do
+      post users_path, params:{user: {name:  "Example User",
+                                      email: "user@example.com",
+                                      password:              "password",
+                                      password_confirmation: "password" }}
+      end.to change{User.count}.by(1)
+      follow_redirect!
+      expect(response).to render_template("users/show")
+    end
+  end
+
+  
 end
