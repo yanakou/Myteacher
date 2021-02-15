adbac0c0cb46545ada0d18276f945aecc338b11d
yanakou <ky50315101@outlook.jp>
2021-02-15 11:32:49 +0900

    #40 update  letを使用してリファクタリング

--- spec/requests/users_signup_spec.rb
+++ spec/requests/users_signup_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "UsersSignup", type: :request do
   describe "GET /signup" do
+    let(:subject){User.create(name:  "",
+                              email: "user@invalid",
+                              password:              "foo",
+                              password_confirmation: "bar" )}
     it "invalid signup information" do
       get signup_path
-      expect do
-        post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
-      end.to_not change{User.count}
+      expect{subject}.to_not change{User.count}
       expect(response).to render_template("users/new")
     end
   end
