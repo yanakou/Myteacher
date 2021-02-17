b9253eb932f37a86ff249993ad80af920ac03c2a
yanakou <ky50315101@outlook.jp>
2021-02-17 22:58:48 +0900

    #60 fix logged_in?メソッドを修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,13 +8,13 @@ module SessionsHelper
   #現在ログイン中のユーザーを返す
   def current_user
     if session[:user_id]
-      @current_user ||= User.find_by(id:session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
   #ログインしている時にtrueを返し、それ以外ならfalseを返す
   def logged_in?
-    !@current_user.nil?
+    !current_user.nil?
   end
   
 end
