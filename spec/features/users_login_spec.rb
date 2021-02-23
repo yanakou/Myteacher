55cc5cf1e54732a502adb7f2c8c546d6e9667b91
yanakou <ky50315101@outlook.jp>
2021-02-23 23:50:36 +0900

    #62 Add featureでログイン時のレイアウト変更をテスト

--- spec/features/users_login_spec.rb
+++ spec/features/users_login_spec.rb
@@ -1,3 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "UsersLogin", type: :feature do
+RSpec.describe "ログイン", type: :feature do
+  before do
+    @user = create(:user)
+  end
+  
+  it "ログインする" do
+    visit login_path
+    fill_in 'session[email]', with: 'user@example.com'
+    fill_in 'session[password]', with: '000000'
+    click_button 'ログイン'
+    expect(page).to have_no_link nil, href: login_path
+    expect(page).to have_link nil, href: logout_path
+    expect(page).to have_link nil, href: user_path(@user)
+  end
+end 
