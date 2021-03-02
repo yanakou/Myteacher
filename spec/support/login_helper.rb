80d7b569ff6b165141191eedf40a615375190481
yanakou <ky50315101@outlook.jp>
2021-03-02 21:57:11 +0900

    #76 Add log_in_asヘルパーメソッドを追加

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -3,7 +3,10 @@ module LoginHelper
     !session[:user_id].nil?
   end
 
-  
+  def log_in_as(user)
+    post login_path, params: { session: { email: user.email,
+                                          password: "000000"}}
+  end
 end
 
   
