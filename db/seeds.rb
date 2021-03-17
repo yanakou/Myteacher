ea41197e287a515893c74e929672eb7f373f54e6
yanakou <ky50315101@outlook.jp>
2021-03-18 07:47:14 +0900

    #111 update seeds.rbのtweetを複数生成し、viewに表示する

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,4 +21,14 @@ Tweet.create!(title:  "Hello",
              text: "I'm feeling good!!",
              image: File.open('./app/assets/images/camera.jpeg'),
              user_id: 1)
-             
+
+30.times do |n|
+  title  = Faker::Lorem.sentence(word_count: 2)
+  text = Faker::Lorem.sentence(word_count: 5)
+  image =  File.open('./app/assets/images/camera.jpeg')
+  user_id = n+1
+  Tweet.create!(title: title,
+               text: text,
+               image: image,
+               user_id: user_id)
+end
