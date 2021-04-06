304b91492bacd614b90b3409fe4aeaa3bd0ba6f8
yanakou <ky50315101@outlook.jp>
2021-04-07 07:56:48 +0900

    127 Add UsersコントローラーのshowアクションをDM用に編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,24 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if logged_in?
+      @currentUserEntry = Entry.where(user_id: current_user.id)
+      @userEntry = Entry.where(user_id: @user.id)
+      unless @user.id == current_user.id
+        @currentUserEntry.each do |cu|
+          @userEntry.each do |u|
+            if cu.room_id == u.room_id
+              @haveRoom = true
+              @roomId = cu.room_id
+            end
+          end
+        end
+        unless @haveRoom
+          @room = Room.new
+          @entry = Entry.new
+        end
+      end
+    end
   end
 
   def create
