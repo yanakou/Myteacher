d600d4c690b77ee5dd096c160f712dbde2187892
yanakou <ky50315101@outlook.jp>
2021-04-11 12:30:28 +0900

    127 Add messagesコントローラーを実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,51 +1,42 @@
 class MessagesController < ApplicationController
-  before_action :set_room, only: [:create, :edit, :update, :destroy]
-  before_action :set_message, only: [:edit, :update, :destroy]
+  before_action :set_room, only: %i[create edit update destroy]
+  before_action :set_message, only: %i[edit update destroy]
 
   def create
-      if Entry.where(user_id: current_user.id, room_id: @room.id)
-          @message = Message.create(message_params)
-              if @message.save
-                  @message = Message.new
-                  gets_entries_all_messages
-              end
-      else
-          flash[:alert] = "メッセージの送信に失敗しました"
-      end
+    @message = Message.create(message_params)
+    if @message.save
+      # @room.create_notification_message!(current_user, @message.id)
+      @message = Message.new
+    end
+    gets_entries_all_messages
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
-      if @message.update(message_params)
-          gets_entries_all_messages
-      end
+    gets_entries_all_messages if @message.update(message_params)
   end
 
   def destroy
-      if @message.destroy
-          gets_entries_all_messages
-      end
+    gets_entries_all_messages if @message.destroy
   end
 
   private
 
-
   def set_room
-      @room = Room.find(params[:message][:room_id])
+    @room = Room.find(params[:message][:room_id])
   end
 
   def set_message
-      @message = Message.find(params[:id])
+    @message = Message.find(params[:id])
   end
 
   def gets_entries_all_messages
-      @messages = @room.messages.includes(:user).order("created_at asc")
-      @entries = @room.entries
+    @messages = @room.messages.includes(:user).order("created_at asc")
+    @entries = @room.entries
   end
 
   def message_params
-      params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id)
+    params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id)
   end
 end
