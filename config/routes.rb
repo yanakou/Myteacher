3d678f499408f61ef46598b880f6ee2ef9a991e7
yanakou <ky50315101@outlook.jp>
2021-04-19 10:29:32 +0900

    #136 Update ルーティングをネストさせる

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,20 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
   resources :users do
     member do
         get :following, :followers
     end
   end
+
   resources :relationships, only: [:create, :destroy]
+
   resources :tweets do 
     resources :comments, only: [:create, :edit, :update, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
+  
   resources :rooms, only: [:index, :create, :show]
   resources :messages, only: [:create, :edit, :update, :destroy]
 end
