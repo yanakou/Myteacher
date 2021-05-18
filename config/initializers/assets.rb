7cbfa9edbc633d626769e97e1a596439945293ed
yanakou <ky50315101@outlook.jp>
2021-05-19 07:54:42 +0900

    #142 Add application.cssのprecompileの設定を追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# application.cssについてのエラー(Sprockets::Rails::Helper::AssetNotPrecompiled in)が出たため、追記
+Rails.application.config.assets.precompile += %w( application.css )
