783478f2837aeb1443e65c0b091b07a13b6148f6
yanakou <ky50315101@outlook.jp>
2021-06-08 07:39:35 +0900

    #150 Notificationモデルとの関連付け(Room側)

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,8 @@
 class Room < ApplicationRecord
+  # messages-------------------------------------------------------------------
   has_many :messages
   has_many :entries
   has_many :users, through: :entries
+  # notifications-------------------------------------------------------------------
+  has_many :notifications
 end
