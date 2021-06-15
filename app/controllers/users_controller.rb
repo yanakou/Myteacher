bcdbd6df31a30fb2c2b24861f367c53aaf432a62
yanakou <ky50315101@outlook.jp>
2021-06-16 07:27:18 +0900

    #154 Update 既存の処理をbeforeアクションに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
-  
+  before_action :join_room, only: [:show, :likes]
 
   def index
     @users = User.all.page(params[:page])
@@ -13,30 +13,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @tweets = @user.tweets.includes(:taggings).order('created_at desc').page(params[:page]).per(10)
     @user = User.find(params[:id])
-    if logged_in?
-      @currentUserEntry = Entry.where(user_id: current_user.id)
-      @userEntry = Entry.where(user_id: @user.id)
-      unless @user.id == current_user.id
-        @currentUserEntry.each do |cu|
-          @userEntry.each do |u|
-            if cu.room_id == u.room_id
-              @haveRoom = true
-              @roomId = cu.room_id
-            end
-          end
-        end
-        unless @haveRoom
-          @room = Room.new
-          @entry = Entry.new
-        end
-      end
-    end
+    @tweets = @user.tweets.includes(:taggings).order('created_at desc').page(params[:page]).per(10)
   end
 
   def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "MyTeacherへようこそ！"
@@ -97,4 +79,24 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user.admin?
   end
 
+  def join_room
+    if logged_in?
+      @currentUserEntry = Entry.where(user_id: current_user.id)
+      @userEntry = Entry.where(user_id: @user.id)
+      unless @user.id == current_user.id
+        @currentUserEntry.each do |cu|
+          @userEntry.each do |u|
+            if cu.room_id == u.room_id
+              @haveRoom = true
+              @roomId = cu.room_id
+            end
+          end
+        end
+        unless @haveRoom
+          @room = Room.new
+          @entry = Entry.new
+        end
+      end
+    end
+  end
 end
