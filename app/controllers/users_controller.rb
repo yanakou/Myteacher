7d6dd725703bc91fb3239a8dface39e1ce0513ea
yanakou <ky50315101@outlook.jp>
2021-06-16 07:44:25 +0900

    #154 Update 既存の処理をbeforeアクションに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   before_action :join_room, only: [:show, :likes]
@@ -13,7 +14,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @tweets = @user.tweets.includes(:taggings).order('created_at desc').page(params[:page]).per(10)
   end
 
@@ -29,11 +29,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "プロフィールを変更しました"
       redirect_to @user
@@ -43,12 +41,10 @@ class UsersController < ApplicationController
   end
 
   def following
-    @user = User.find(params[:id])
     @users = @user.following
   end
 
   def followers
-    @user = User.find(params[:id])
     @users = @user.followers
   end
 
