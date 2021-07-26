8a670c6607af2796ab1ad03f09f720f7d49c08fd
yanakou <ky50315101@outlook.jp>
2021-07-26 19:51:11 +0900

    #229 Add 検索機能関係のテストを追加

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,49 +1,96 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
+  describe '#create' do
+    context "tweetを保存できる場合" do
+      it "投稿が可能である" do
+        @tweet = create(:tweet)
+        expect(@tweet).to be_valid
+      end
 
-  context "tweetを保存できる場合" do
-    it "投稿が有効であること" do
-      @tweet = create(:tweet)
-      expect(@tweet).to be_valid
+      it "imageがnilでもtweetを保存できる" do
+        @tweet = create(:tweet, image: nil)
+        expect(@tweet).to be_valid
+      end
     end
 
-    it "imageがnilでもtweetを保存できること" do
-      @tweet = create(:tweet, image: nil)
-      expect(@tweet).to be_valid
+    context "tweetを保存できない場合" do
+      it "textとtitleの両方が空の場合は保存できない" do
+        @tweet = build(:tweet, title: nil, text: nil)
+        expect(@tweet).not_to be_valid
+      end
+
+      it "タイトルが空の場合は保存できない" do
+        @tweet = build(:tweet, title: nil)
+        expect(@tweet).not_to be_valid
+      end
+
+      it "タイトルが41文字以上の場合は保存できない" do
+        @tweet = build(:tweet, title: Faker::Lorem.characters(number: 41))
+        expect(@tweet).not_to be_valid
+      end
+
+
+      it "textが空の場合は保存できない" do
+        @tweet = build(:tweet, text: nil)
+        expect(@tweet).not_to be_valid
+      end
+
+      it "textが1501文字以上の場合は保存できない" do
+        @tweet = build(:tweet, text: Faker::Lorem.characters(number: 1501))
+        expect(@tweet).not_to be_valid
+      end
+
+      it "user_idが空の場合は保存できない" do
+        @tweet = build(:tweet, user_id: nil)
+        expect(@tweet).not_to be_valid
+      end
     end
   end
+  
+  describe "#search" do
+    # 各テストの前にTweetを作成
+    before do
+      user = create(:user)
+      @tweet = create(
+        :tweet,
+        title: "天気が良い",
+        text: "今日は快晴です",
+        user: user
+      )
 
-  context "tweetを保存できない場合" do
-    it "textとtitleの両方が空の場合は保存できない" do
-      @tweet = build(:tweet, title: nil, text: nil)
-      expect(@tweet).not_to be_valid
+      @other_tweet = create(
+        :tweet,
+        title: "天気が悪い",
+        text: "今日は雨です",
+        user: user
+      )
     end
 
-    it "タイトルが空でないこと" do
-      @tweet = build(:tweet, title: nil)
-      expect(@tweet).not_to be_valid
+    context "「晴」で検索した場合、曖昧検索できているか" do
+      it "@tweetを返すこと" do
+        expect(Tweet.search("晴")).to include(@tweet)
+      end
+
+      it "@other_tweetを返さないこと" do
+        expect(Tweet.search("晴")).to_not include(@other_tweet)
+      end
     end
 
-    it "タイトルが41文字以上の場合は保存できないこと" do
-      @tweet = build(:tweet, title: Faker::Lorem.characters(number: 41))
-      expect(@tweet).not_to be_valid
+    context "「快晴」で検索した場合、一致検索できているか" do
+      it "@tweetを返すこと" do
+        expect(Tweet.search("快晴")).to include(@tweet)
+      end
+
+      it "@other_tweetを返さないこと" do
+        expect(Tweet.search("快晴")).to_not include(@other_tweet)
+      end
     end
 
-
-    it "textが空でないこと" do
-      @tweet = build(:tweet, text: nil)
-      expect(@tweet).not_to be_valid
+    context "検索に一致しないものは表示されないこと" do
+      it "「曇り」で検索した場合、0件であること" do
+        expect(Tweet.search("曇り")).to be_empty
+      end
     end
-
-    it "textが1501文字以上の場合は保存できないこと" do
-      @tweet = build(:tweet, text: Faker::Lorem.characters(number: 1501))
-      expect(@tweet).not_to be_valid
-    end
-
-    it "user_idtが空でないこと" do
-      @tweet = build(:tweet, user_id: nil)
-      expect(@tweet).not_to be_valid
-    end
-  end 
+  end
 end
