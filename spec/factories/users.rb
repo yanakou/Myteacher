a6c107e21ba9be2ffff58d3f24fe5b5a5d817cb3
yanakou <ky50315101@outlook.jp>
2021-07-27 22:32:15 +0900

    #229 Update ユーザーデータのnameとemailをランダムに生成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name {'user'}
-    email {'user@example.com'}
+    name {Faker::Name.last_name}
+    email {Faker::Internet.email}
     password {'000000'}
     password_confirmation {'000000'}
     avatar { File.open("#{Rails.root}/app/assets/images/default.png") }
