5c7b24b817553dd0aa26db74db45290a2639ae38
yanakou <ky50315101@outlook.jp>
2021-07-31 07:37:53 +0900

    #241 Add コメントの上限を1000文字にする

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :tweet
   belongs_to :user
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 1000 }
   # notifications-------------------------------------------------------------------
   has_many :notifications, dependent: :destroy
   
