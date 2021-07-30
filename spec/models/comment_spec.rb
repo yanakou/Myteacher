d2120cea476db4da3ef0cd97ddaf04322247bb9f
yanakou <ky50315101@outlook.jp>
2021-07-31 07:58:40 +0900

    #241 Add Commentモデルのテストを追加

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:comment) { create(:comment) }
+  describe '#create' do
+    context '保存できる場合' do
+      it "全てのパラメーターが揃っていれば保存できる" do
+        expect(comment).to be_valid
+      end
+
+      it "contentが1文字以上なら保存できる" do
+        comment.content = "A" * 1
+        expect(comment).to be_valid
+      end
+
+      it "contentが1,000文字以内なら保存できる" do
+        comment.content = "A" * 1000
+        expect(comment).to be_valid
+      end
+    end
+
+    context "保存できない場合" do
+      it "user_idがnilの場合は保存できない" do
+        comment.user_id = nil
+        comment.valid?
+        expect(comment.errors[:user]).to include("を入力してください")
+      end
+
+      it "tweet_idがnilの場合は保存できない" do
+        comment.tweet_id = nil
+        comment.valid?
+        expect(comment.errors[:tweet]).to include("を入力してください")
+      end
+
+      it "contentがnilの場合は保存できない" do
+        comment.content = nil
+        comment.valid?
+        expect(comment.errors[:content]).to include("を入力してください")
+      end
+
+      it "contentが1001文字以上の場合は保存できない" do
+        comment.content = "A" * 1001
+        comment.valid?
+        expect(comment.errors[:content]).to include("は1000文字以内で入力してください")
+      end
+    end
+  end
+
+  describe "各モデルとのアソシエーション" do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+    
+    context "Userモデルとのアソシエーション" do
+      let(:target) { :user }
+      it "Userとの関連付けはbelongs_toであること" do
+        expect(association.macro).to  eq :belongs_to
+      end
+    end
+
+    context "Tweetモデルとのアソシエーション" do
+      let(:target) { :tweet }
+      it "Tweetとの関連付けはbelongs_toであること" do
+        expect(association.macro).to  eq :belongs_to
+      end
+    end
+
+    context "Notificationモデルとのアソシエーション" do
+      let(:target) { :notifications }
+
+      it "Notificationとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :has_many
+      end
+
+      it "Commentが削除されたらNotificationも削除されること" do
+        notification = create(:notification, comment_id: comment.id, visitor_id: 1, visited_id: 1)
+        expect { comment.destroy }.to change(Notification, :count).by(-1)
+      end
+    end
+  end
 end
