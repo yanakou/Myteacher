7784dac0b11f39ffbddaf683b005b00556bd3237
yanakou <ky50315101@outlook.jp>
2021-08-02 23:00:51 +0900

    #245 Add indexとshowアクションのテストを追加

---
+++ app/javascript/preview.js
@@ -0,0 +1,38 @@
+$(function () {
+  // previewボタンが押されたらイベント発火
+  $("#preview").on("click", function () {
+    var text = $("#md-textarea").val();
+    if (text == "") {
+      return false;
+    } else if ($("#md-textarea").val().length > 1000) {
+      alert("本文を1,000文字以内で入力してください。");
+      return false;
+    }
+    // ajax通信を開始。
+    $.ajax({
+      url: "/api/tweets/preview", //apiフォルダのtweetsコントローラーのpreviewメソッドを参照
+      type: "GET",
+      dataType: "json",
+      data: { text: text },
+    })
+      .done(function (html) {
+        // ajax成功したら、テキストエリアを非表示にする。
+        $("#md-textarea").parent().css("display", "none");
+        $("#preview-area").show().empty().append(html.text);
+        // markdownボタンとpreviewボタンのdisabledを入れ替える。
+        $("#markdown").removeClass("disabled");
+        $("#preview").addClass("disabled");
+      })
+      .fail(function () {
+        alert("1,000文字以内で入力してください。");
+      });
+  });
+
+  // markdownボタンが押されたらイベント発火
+  $("#markdown").on("click", function () {
+    $("#md-textarea").parent().css("display", "");
+    $("#preview-area").hide();
+    $("#preview").removeClass("disabled");
+    $("#markdown").addClass("disabled");
+  });
+});
---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,138 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe 'GET #index' do
+    before do
+      user2 = create(:user2)
+      log_in_as(user2)
+    end
+
+    it 'リクエストが成功すること' do
+      get users_path
+      expect(response.status).to eq 200
+    end
+
+    it 'ユーザー名が表示されていること' do
+      get users_path
+      expect(response.body).to include ("user2")
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      @user2 = create(:user2)
+      log_in_as(@user2)
+    end
+
+    it 'リクエストが成功すること' do
+      get user_path(@user2.id)
+      expect(response.status).to eq 200
+    end
+
+    it 'ユーザー名が表示されていること' do
+      get user_path(@user2.id)
+      expect(response.body).to include ("user2")
+    end
+
+    context 'ユーザーが存在しない場合' do
+      subject { -> { get user_path 1} }
+
+      it { is_expected.to raise_error ActiveRecord::RecordNotFound }
+    end
+  end
+
+
+  # before do
+  #   @user = create(:user)
+  #   @other_user = create(:user2)
+  # end
+
+  # describe "GET /signup" do
+  #   it "ログインページの表示に成功すること" do
+  #     get signup_path
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET /users" do
+  #   context "非ログイン時" do
+  #     it "ユーザー一覧ページにアクセスすると、ログインページにリダイレクトすること" do
+  #       get users_path
+  #       expect(response).to redirect_to login_url
+  #     end
+  #   end
+  # end
+
+  # describe "GET /users/:id/edit" do
+  #   context "非ログイン時" do
+  #     it "編集ページにアクセスすると、ログインページにリダイレクトすること" do
+  #       get edit_user_path(@user)
+  #       expect(flash).to_not be_empty
+  #       expect(response).to redirect_to login_url
+  #     end
+  #   end
+
+  #   context "異なるユーザーでログインを試みた場合" do
+  #     it "編集ページにアクセスすると、ルートurlにリダイレクトすること" do
+  #       log_in_as(@other_user)
+  #       get edit_user_path(@user)
+  #       expect(flash).to be_empty
+  #       expect(response).to redirect_to root_url
+  #     end
+  #   end
+  # end
+
+  # describe "PATCH /users/:id" do
+  #   context "非ログイン時" do
+  #     it "編集を試みると、ログインページにリダイレクトすること" do
+  #       patch user_path(@user), params: { user: { name: @user.name,
+  #                                             email: @user.email } }
+  #       expect(flash).to_not be_empty
+  #       expect(response).to redirect_to login_url
+  #     end
+  #   end
+
+  #   context "異なるユーザーでログインを試みた場合" do
+  #     it "編集を試みると、ルートurlにリダイレクトすること" do
+  #       log_in_as(@other_user)
+  #       patch user_path(@user), params: { user: { name: @user.name,
+  #                                                 email: @user.email } }
+  #       expect(flash).to be_empty
+  #       expect(response).to redirect_to root_url
+  #     end
+  #   end
+
+  #   context "管理者でないユーザーがログイン" do
+  #     it "admin属性を変更できないこと" do
+  #       log_in_as(@other_user)
+  #       expect(@other_user.admin?).to be_falsey 
+  #       patch user_path(@other_user), params: {user: { password: '000000',
+  #                                           password_confirmation: '000000',
+  #                                           admin: true } }
+  #       expect(@other_user.reload.admin?).to be_falsey
+  #     end
+  #   end
+  # end
+
+  # describe "DELETE /users/:id" do
+  #   context "非ログイン時" do
+  #     it "ユーザーの削除に失敗し、ログインページにリダイレクトすること" do
+  #       expect do
+  #         delete user_path(@user)
+  #       end.to_not change{User.count}
+  #       expect(response).to redirect_to login_url
+  #     end
+  #   end
+
+  #   context "管理者でないユーザーがログイン" do
+  #     it "ユーザーの削除に失敗し、ログインページにリダイレクトすること" do
+  #       log_in_as(@other_user)
+  #       expect do 
+  #         delete user_path(@user)
+  #       end.to_not change{User.count}
+  #       expect(response).to redirect_to root_url
+  #     end
+  #   end
+  # end
+
+end
