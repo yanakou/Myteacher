c71fee0d781c61251d7130f4e4479f46318ed41b
yanakou <ky50315101@outlook.jp>
2021-08-15 21:23:40 +0900

    #255 Add editアクションのテストを追加

--- spec/requests/comments_request_spec.rb
+++ spec/requests/comments_request_spec.rb
@@ -34,4 +34,21 @@ RSpec.describe "Comments", type: :request do
       end
     end
   end
+
+  describe 'GET #edit' do
+    before do
+      @comment = create(:comment)
+      @tweet = @comment.tweet
+    end
+
+    it 'リクエストが成功すること' do
+      get edit_tweet_comment_path(@tweet.id, @comment.id), xhr: true
+      expect(response.status).to eq 200
+    end
+
+    it 'contentが表示されていること' do
+      get edit_tweet_comment_path(@tweet.id, @comment.id), xhr: true
+      expect(response.body).to include @comment.content
+    end
+  end
 end
