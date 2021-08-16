7f6dc33fec9e57e0c43694b0f46c201c2d1e3916
yanakou <ky50315101@outlook.jp>
2021-08-16 21:55:46 +0900

    #259 Add destroyアクションのテストを追加

--- spec/requests/likes_request_spec.rb
+++ spec/requests/likes_request_spec.rb
@@ -21,5 +21,22 @@ RSpec.describe "Likes", type: :request do
   end
 
   describe 'DELETE #destroy' do
+    before do
+      @like = create(:like)
+      @tweet = @like.tweet
+      @user = @like.user
+      log_in_as(@user)
+    end
+
+    it 'リクエストが成功すること' do
+      delete tweet_like_path(@tweet.likes, tweet_id: @tweet.id), xhr: true
+      expect(response.status).to eq 200
+    end
+
+    it 'いいねが削除されること' do
+      expect do
+        delete tweet_like_path(@tweet.likes, tweet_id: @tweet.id), xhr: true
+      end.to change(Like, :count).by(-1)
+    end
   end
 end
