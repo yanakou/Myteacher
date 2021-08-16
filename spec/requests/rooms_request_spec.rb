5e0460b23d2f8ebfa6b238c46057efc3c3f85298
yanakou <ky50315101@outlook.jp>
2021-08-16 18:37:21 +0900

    #257 Add showアクションのテストを追加

--- spec/requests/rooms_request_spec.rb
+++ spec/requests/rooms_request_spec.rb
@@ -63,4 +63,53 @@ RSpec.describe "Rooms", type: :request do
       end
     end
   end
+
+  describe 'GET #show' do
+    let!(:user){create(:user)}
+    let!(:user2){create(:user2)}
+
+    context 'ログインしている場合' do
+      before do
+        log_in_as(user)
+        @room = create(:room)
+        @message = create(:message, user_id: user2.id, room_id: @room.id, message: 'Hello!')
+        @entry1 = create(:entry1, user_id: user.id, room_id: @room.id)
+        @entry2 = create(:entry1, user_id: user2.id, room_id: @room.id)
+      end
+
+      it 'リクエストが成功すること' do
+        get room_path(@room.id)
+        expect(response.status).to eq 200
+      end
+
+      it '相手の名前が表示されていること' do
+        get room_path(@room.id)
+        expect(response.body).to include 'user2'
+      end
+
+      it 'メッセージ内容が表示されていること' do
+        get room_path(@room.id)
+        expect(response.body).to include 'Hello!'
+      end
+    end
+
+    context 'ログインしていない場合' do
+      before do
+        @room = create(:room)
+        @message = create(:message, user_id: user2.id, room_id: @room.id, message: 'Hello!')
+        @entry1 = create(:entry1, user_id: user.id, room_id: @room.id)
+        @entry2 = create(:entry1, user_id: user2.id, room_id: @room.id)
+      end
+
+      it 'リクエストは302 OKとなること' do
+        get room_path(@room.id)
+        expect(response.status).to eq 302
+      end
+
+      it 'ログイン画面にリダイレクトすること' do
+        get room_path(@room.id)
+        expect(response).to redirect_to login_url
+      end
+    end
+  end
 end
