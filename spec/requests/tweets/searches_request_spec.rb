cfb80e79a17ce1d9218ece2e07465b3bf35d71fc
yanakou <ky50315101@outlook.jp>
2021-08-21 07:34:02 +0900

    #263 Add indexアクションのテストを追加

--- spec/requests/tweets/searches_request_spec.rb
+++ spec/requests/tweets/searches_request_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Tweets::Searches", type: :request do
-
   describe "GET /index" do
-    # it "returns http success" do
-    #   get "/tweets/searches/index"
-    #   expect(response).to have_http_status(:success)
-    # end
-  end
+    let!(:tweet1){create(:tweet1)}
+    context '検索内容がヒットする場合' do
+      it 'リクエストが成功すること' do
+        get tweets_searches_path, params: { keyword: 'tweet1' }
+        expect(response.status).to eq 200
+      end
 
+      it 'titleが表示されていること' do
+        get tweets_searches_path, params: { keyword: 'tweet1' }
+        expect(response.body).to include ("tweet1")
+      end
+    end
+
+    context '検索内容がヒットしない場合' do
+      it 'リクエストが成功すること' do
+        get tweets_searches_path, params: { keyword: 'tweet2' }
+        expect(response.status).to eq 200
+      end
+
+      it '検索内容不一致の結果が表示されていること' do
+        get tweets_searches_path, params: { keyword: 'tweet2' }
+        expect(response.body).to include ("一致する投稿はありません")
+      end
+    end
+  end
 end
