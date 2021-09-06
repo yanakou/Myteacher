932babf13664f089ae4d89df6ab0f8fb42c62d10
yanakou <ky50315101@outlook.jp>
2021-09-06 21:49:50 +0900

    Update 本番環境の設定に変更

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,20 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIAVOFJAHPFJSHWYUHA',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'myt-aws-bucket'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/myt-aws-bucket'
+  end
+end
