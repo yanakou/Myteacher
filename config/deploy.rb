beef87bdbd2bb2afac13c4269cf512f59cbe5499
yanakou <ky50315101@outlook.jp>
2021-09-11 21:02:50 +0900

    #278 Update デプロイ時に行われるタスクを設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,62 +1,42 @@
 # config valid for current version and patch releases of Capistrano
 lock "3.16.0"
+
 # デプロイするアプリケーション名
 set :application, "Myteacher"
+
 # cloneするgitのレポジトリ
 set :repo_url, "git@github.com:yanakou/Myteacher.git"
-# deployするブランチ。デフォルトはmasterなのでなくても可。
-set :branch, 'master'
-# deploy先のディレクトリ。
-set :deploy_to, '/var/www/Myteacher'
-# シンボリックリンクをはるファイル
-set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
-# シンボリックリンクをはるフォルダ。(※後述)
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-# 保持するバージョンの個数(※後述)
+
+# sharedディレクトリに入れるファイルを指定
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
+# SSH接続設定
+set :ssh_options, {
+  auth_methods: ['publickey'], 
+  keys: ['~/.ssh/myt_key_rsa'] 
+}
+
+# 保存しておく世代の設定
 set :keep_releases, 5
 
-# rubyのバージョン
+# rbenvの設定
+set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
-#出力するログのレベル。
-set :log_level, :debug
+# ここからUnicornの設定
+# Unicornのプロセスの指定
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
+# Unicornの設定ファイルの指定
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+
+# Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-  desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
   end
-
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
-        end
-      end
-    end
-  end
-
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-    end
-  end
 end
-
 
 
 # Default branch is :master
