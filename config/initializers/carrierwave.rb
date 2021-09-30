caceea9fc6b1d9a5cd7dbcefd30018dbf39e9111
yanakou <ky50315101@outlook.jp>
2021-09-30 23:56:52 +0900

    #286 Update dockerの導入

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,6 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  else
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
