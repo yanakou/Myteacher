c7f62180e72b9cb4f55b5d7622198ee25710f616
yanakou <ky50315101@outlook.jp>
2021-10-01 20:55:49 +0900

    fix S3にアップデートできないエラーを解消

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,17 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-    config.storage = :fog
+  if Rails.env.development? || Rails.env.test?
+    config.cache_storage = :file
+  else
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAVOFJAHPFJSHWYUHA',
-      aws_secret_access_key: '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',
+      aws_access_key_id: 'AKIAVOFJAHPFGFENZ7HS',
+      aws_secret_access_key: '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',
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'myt-aws-bucket'
-    
+    config.fog_directory  = 'myt-aws-bucket-2'
+    config.cache_storage = :fog
   end
 end
